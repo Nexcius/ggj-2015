de70e494da3ebb5cd849c4afbee3a80286311de4
Håvard Kindem <nex@nexcius.net>
2015-01-23 23:48:16 +0100

    Initial Commit

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Entity : MonoBehaviour {
+	protected Vector2 velocity = new Vector2();
+
+
+	protected virtual void Start () {
+
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		transform.position = transform.position + (Vector3)(velocity * Time.deltaTime);
+	}
+
+	public bool IsAirbound() {
+		return Mathf.Abs(rigidbody2D.velocity.y) > 0.001f;
+	}
+}
---
+++ Assets/Scripts/Retricle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Retricle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 mousePos = Input.mousePosition;
+		if (Input.mousePosition != null) {
+			Vector2 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			transform.position = pos;
+		} 	
+	}
+
+	void OnGUI() {
+
+	}
+}
