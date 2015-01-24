9932512952ff6209786b4ad4e148f96d99b016ca
Håvard <nex@nexcius.net>
2015-01-24 12:56:36 +0100

    asd

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -27,4 +27,6 @@ public class MonsterController : Entity {
         base.FixedUpdate();
         
     }
+
+    
 }
---
+++ Assets/Scripts/PlayerRules.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRules : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(collision.gameObject.layer);
+        Debug.Log("ASD");
+    }
+}
