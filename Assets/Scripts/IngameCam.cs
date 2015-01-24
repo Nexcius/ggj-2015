9c95c48533c1d6dd9f80eca0e77521d9f8365c12
Håvard Kindem <nex@nexcius.net>
2015-01-24 23:50:29 +0100

    Added napoleon animation

--- Assets/Scripts/IngameCam.cs
+++ Assets/Scripts/IngameCam.cs
@@ -1,21 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class IngameCam : MonoBehaviour {
 
 	[SerializeField]
-	protected float duration = 9.0f;
+	protected float sfinxDuration = 9.0f;
+
+	[SerializeField]
+	protected float napoleonDuration = 9.0f;
+
+	private float duration = 0.0f;
+	private string clip = "sfinx";
 
 	private float startTime = 0.0f;
+	Animator _animator;
+
+	void Start() {
+		_animator = GetComponent<Animator>();
+		gameObject.SetActive (false);
+	}
 	
 	// Update is called once per frame
 	void Update () {
+		//_animator.Play(Animator.StringToHash("sfinx"));
+		_animator.Play(Animator.StringToHash(clip));
 		if(startTime + duration < Time.time) {
 			gameObject.SetActive(false);
 		}
 	}
 
+
 	public void Play() {
+		if(gameObject.activeSelf) {
+			return;
+		}
+
+		if(Random.value > 0.5f) {
+			duration = napoleonDuration;
+			clip = "napoleon";
+		}
+		else {
+			duration = sfinxDuration;
+			clip = "sfinx";
+		}
+
 		startTime = Time.time;
 		gameObject.SetActive (true);
 	}
